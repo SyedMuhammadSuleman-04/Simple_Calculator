54092c065d0d5d53070a9a0e46d8e0b3468db955
Syed Muhammad Suleman <sulemansyedmuhammad41@gmail.com>
2024-03-22 16:01:32 +0500

    push my calculator to github

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number:", type: "number", name: "firstNumber" },
+    { message: "Enter second number:", type: "number", name: "secondNumber" },
+    {
+        message: "Select operator from following operation:",
+        type: "list",
+        name: "operators",
+        choices: ["Add", "Subtract", "Multiply", "Divide", "Power"],
+    },
+]);
+if (answer.operators === "Add") {
+    console.log(answer.firstNumber + answer.secondNumber);
+    console.log("Superb!");
+    console.log("Keep it up!");
+}
+else if (answer.operators === "Subtract") {
+    console.log(answer.firstNumber - answer.secondNumber);
+    console.log("Superb!");
+    console.log("Keep it up!");
+}
+if (answer.operators === "Multiply") {
+    console.log(answer.firstNumber * answer.secondNumber);
+    console.log("Superb!");
+    console.log("Keep it up!");
+}
+else if (answer.operators === "Divide") {
+    console.log(answer.firstNumber / answer.secondNumber);
+    console.log("Superb!");
+    console.log("Keep it up!");
+}
+else if (answer.operators === "Power") {
+    console.log(answer.firstNumber ** answer.secondNumber);
+    console.log("Superb!");
+    console.log("Keep it up!");
+}
+;
